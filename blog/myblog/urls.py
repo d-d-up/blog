12e9767ca3e85a5489fa6134a15ad16fc63e7976
d-d-up <qhr6113@163.com>
2018-08-30 09:31:10 +0800

    增加分类列表显示

--- blog/myblog/urls.py
+++ blog/myblog/urls.py
@@ -1,11 +1,13 @@
 # -*- coding:utf-8 -*-
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
-from .views import MyBlogListView, BlogContentCreateView, BlogContentDetailView
+from .views import MyBlogListView, BlogContentCreateView, BlogContentDetailView, BlogSortContentListView
 from .forms import LoginForm
 urlpatterns = [
     url(r'^$', MyBlogListView.as_view(), name='myblog_list'),
     url(r'^create/$', BlogContentCreateView.as_view(), name='myblog_create'),
     url(r'^detail/(?P<pk>[\d]+).html$', BlogContentDetailView.as_view(), name='myblog_detail'),
     url(r'^accounts/login/$', auth_views.LoginView.as_view(template_name='login.html', authentication_form=LoginForm), name='blog_login'),
+    url(r'^sort/(?P<pk>[\d]+).html', BlogSortContentListView.as_view(), name='sort_content'),
+    
 ]
--- blog/myblog/views.py
+++ blog/myblog/views.py
@@ -18,6 +18,13 @@ class MyBlogListView(ListView):
     paginate_by = 5 
     context_object_name = 'blog_contents'
 
+    def get_context_data(self, **kwargs):
+        """重写上下文函数，加入自己的内容."""
+        context = super(MyBlogListView, self).get_context_data(**kwargs)
+        sort_list = BlogSort.objects.all()
+        context['sort_list'] = sort_list 
+        return context
+
 
 class BlogContentCreateView(LoginRequiredMixin, CreateView):
     """博客内容创建"""
@@ -32,4 +39,33 @@ class BlogContentDetailView(DetailView):
     template_name = "myblog_detail.html"
     context_object_name = "blog_content"
 
+    def get_context_data(self, **kwargs):
+        """重写上下文函数，加入自己的内容."""
+        context = super(BlogContentDetailView, self).get_context_data(**kwargs)
+        sort_list = BlogSort.objects.all()
+        context['sort_list'] = sort_list 
+        return context
 
+
+class BlogSortContentListView(ListView):
+    """博客分类内容列表"""
+    model = BlogSort
+    template_name = "myblog_list.html"
+    context_object_name = 'blog_contents'
+    paginate_by = 5
+
+    def get_queryset(self):
+        """重写."""
+        sort_id = self.kwargs.get('pk')
+
+        blog_sort_objects = BlogContent.objects.all()
+        if sort_id:
+            blog_sort_objects = blog_sort_objects.filter(sort_id=sort_id)
+        return blog_sort_objects.all()
+
+    def get_context_data(self, **kwargs):
+        """重写上下文函数，加入自己的内容."""
+        context = super(BlogSortContentListView, self).get_context_data(**kwargs)
+        sort_list = BlogSort.objects.all()
+        context['sort_list'] = sort_list 
+        return context
